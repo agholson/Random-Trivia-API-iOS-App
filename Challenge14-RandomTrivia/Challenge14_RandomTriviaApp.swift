3526f136105e71fc58ad76e5460377a3657180ff
agholson <87455899+agholson@users.noreply.github.com>
2021-11-02 21:23:44 -0400

    Initial Commit

---
+++ Challenge14-RandomTrivia/Challenge14_RandomTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge14_RandomTriviaApp.swift
+//  Challenge14-RandomTrivia
+//
+//  Created by Shepherd on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge14_RandomTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
